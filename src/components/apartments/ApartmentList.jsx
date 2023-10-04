242346bb75efe87309f1200e02940185c9878114
leo.genuit <leo.genuit@thetribe.io>
2023-10-04 10:17:03 +0200

    create project

---
+++ src/components/apartments/ApartmentList.jsx
@@ -0,0 +1,12 @@
+import "./ApartmentList.scss";
+export default function ApartmentList() {
+  return (
+    <>
+      <h1>Liste des appartements</h1>
+      <div className="card">
+        <p>image</p>
+        <p>prix par mois</p>
+      </div>
+    </>
+  );
+}
